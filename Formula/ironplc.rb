29059fd03933d4435b898e29dbaebd0e8de6c31a
garretfick <garretfick@users.noreply.github.com>
2026-01-05 19:39:45 +0000

    deploy: ironplc/ironplc@baadadb69111d7910d93b056abb236c3dd6a69b5

--- Formula/ironplc.rb
+++ Formula/ironplc.rb
@@ -14,17 +14,17 @@
 # The formula assumes releases are from the GitHub ironplc/ironplc repository
 # and that releases are prefixed with "v".
 class Ironplc < Formula
-    version "0.148.0"
+    version "0.149.0"
     desc "IronPLC Compiler"
     homepage "https://www.ironplc.com"
     license = "MIT"
   
     if OS.mac?
-        url "https://github.com/ironplc/ironplc/releases/download/v0.148.0/ironplcc-x86_64-macos.tar.gz"
-        sha256 "3541eae4ad0e5346eb15181c2ba5f939ec305bdb70ee0bfa36de4874c50d0455"
+        url "https://github.com/ironplc/ironplc/releases/download/v0.149.0/ironplcc-x86_64-macos.tar.gz"
+        sha256 "24f03d60b4e764bd12633c7084631c415a45b7bb28188b9abaeaff3d30af0d4b"
     elsif OS.linux?
-        url "https://github.com/ironplc/ironplc/releases/download/v0.148.0/ironplcc-x86_64-linux-musl.tar.gz"
-        sha256 "3231a319585018ecda07858a3d7948fe77160e40fde2cab00824a8f937ac6acd"
+        url "https://github.com/ironplc/ironplc/releases/download/v0.149.0/ironplcc-x86_64-linux-musl.tar.gz"
+        sha256 "5c78ef9dcde15031f74846c3ae007d7fc0bf677a09ded5d8f8306bffd79b6443"
     end
   
     def install
